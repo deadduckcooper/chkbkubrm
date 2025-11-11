4aabf8f17f8d0e7b262594abaa3bcdd079be80dc
Ryan Cooper <deadduckcooper@gmail.com>
2025-11-11 08:26:12 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# About chkbkubrm
+A Unique way to report on the results of a BRMS save job on an IBM i system by filter out the messages you expect to see. This resutls in only showing you messages you did not expect to see.
+
+## Origins
+I created this script around the year 2021 to save time seaching thru job logs for problems, and noticing operations staff missing problems. This script is intended to run after each BRMS save job either as an *EXIT at the end of the control group. However I have seen people run it from the scheduler and manually. This script could also be used to search any job log, not just a BRMS save job log.
+
+## Warning
+This is my first time using git and at the time of writing this im not sure how you will download this to the IFS of an IBM i system.  The instructions below were created well before git was involved and assumed someone manually transfering the script to the IFS.
+
+## Instructions
+When calling this program you have the option to pass the parms of
+- LAST This will query the BRMS database for the last save job that has run. Example chkbkubrm.sh LAST
+- Specific job information. Example chkbkubrm.sh 123456/USER/WEEKLY
+- CTLG + Name of BRMS control group and that save will run now. Example chkbkubrm.sh CTLG WEEKLY
+
+## Tips for Setting the Special Variables
+The key to quickly finding problems in the job log is to properally identify message id's that you don't care about. To do this can take a little bit of time, but once set will save you time, and help you resolve problems with your saves. 
+
+After you have the script in the IFS update your email address in the Specail Vars section and run the script as is with no filtering. Review the msgidlist.txt file, this file contains a uniq list of every message id from your saves job log, sorted by servity. Review each message id in the full job log file and decide if that is a message you care to see every time the save completes with errors or fails.  Once done, use your list of message id's you don't care about and set the omitjoblog, omithstlog, and omitbrmlog variables.
+
+You can also set a servity filter for each as you like. Keep in mind some messages you care about might have a low servity, one such example is listed at the end of this document.
+
+## Setup
+From qsh, create the directory structor for this script. I use the directory "/scripts/chkbkubrm"
+```
+mkdir /scripts
+```
+```
+mkdir /scripts/chkbkubrm
+```
+```
+cd /scripts/chkbkubrm
+```
+```
+touch chkbkubrm.sh
+```
+```
+setccsid 1208 chkbkubrm.sh
+```
+Download the chkbkubrm script and replace the file in the IFS you just created, or copy and paste the text into our IFS file. Again, as of now i'm not sure how to get the file to the IFS via git. Once I figure that out I will updated this file.
+                                                 
+## Manually test commands from IBM ACS Run SQL Scripts to make sure they work before running the script.
+```
+CL: DSPOBJD OBJ(QUSRBRM/LOG_INFO) OBJTYPE(*FILE);
+```
+```
+SELECT DISTINCT QUALIFIED_JOB_NAME, MESSAGE_TIMESTAMP FROM QUSRBRM.BRMS_LOG_INFO WHERE AREA ='BACKUP' AND MESSAGE_TIMESTAMP >= current timestamp - 1 day
+ORDER BY MESSAGE_TIMESTAMP DESC LIMIT 1;
+```
+
+Replace $JOBINFO with the qualified job name returned from the previous command for the following SQL statments.
+
+Check history log
+```
+SELECT MESSAGE_ID, SEVERITY, MESSAGE_TIMESTAMP, FROM_JOB, MESSAGE_TEXT FROM table(qsys2.history_log_info(CURRENT TIMESTAMP - 1 DAY)) WHERE FROM_JOB ='$JOBINFO' AND MESSAGE_ID NOT IN('CPF0000') AND SEVERITY >=0 LIMIT 1;
+```
+Check BRMS log
+```
+SELECT MESSAGE_ID, MESSAGE_SEVERITY, QUALIFIED_JOB_NAME, CONTROL_GROUP, MESSAGE_TEXT FROM QUSRBRM.BRMS_LOG_INFO WHERE QUALIFIED_JOB_NAME='$JOBINFO' AND MESSAGE_ID NOT IN('CPF0000') AND SEVERITY >=0 LIMIT 1;
+```
+Check read of spool file job log
+```
+SELECT SPOOLED_FILE_NAME, FILE_NUMBER FROM QSYS2.OUTPUT_QUEUE_ENTRIES WHERE JOB_NAME='$JOBINFO';
+```
+Use your email address and make sure you recieve the test email.
+```
+CL: SNDSMTPEMM RCP(email.address1@example.com) SUBJECT('Test') NOTE('Test');
+```
+
+## Example as to why we don't just filter for greater than SEV x.  You might miss things you care about.
+```
+Message ID . . . . . . . . : BRM1100       Severity . . . . . . . : 10
+Message . . . . :   IPL cannot be started.
+Cause . . . . . :   Current time 23:48 is not within IPL limits 00:01 and 07:00.              
+```
---
+++ chkbkubrm.sh
@@ -0,0 +1,238 @@
+# @(#) Version 2021.12.27
+#
+# When calling this program you have the option to pass the parms of
+# LAST This will query the BRMS database for the last save job that has run
+# Specific job information. Example 980949/SIRIRDC/RYAN
+# No parm assumes you are specifing a BRMS control group as var ctlg within this script below and that save will run now.
+
+# IFS working directory. Location of script and assocated files
+ifsdir=`pwd`
+
+# Vars
+ctlg=RYAN                        #Name of BRMS Control Group to run
+log=strbkubrm.log                #Main log file for this job
+out=strbkubrm.out                #Used to capture the job information
+jobuc=''                         #Upper case job information used in SQL statments
+joblc=''                         #Lower case job informaton used in qsh statments
+file1=fulljoblog.txt             #Spool file with junk removed from it
+file2=dsjoblog.txt               #Spool file with junk removed and blank spaces added before each MSGID
+joblog=joblog.txt                #Job Log results
+joblogtmp=joblog.tmp             #Temp file
+archivejoblog=archivejoblog.txt  #Historical archive Job Log
+hstlog=hstlog.txt                #History Log results
+hstlogtmp=hstlog.tmp             #Temp file
+archivehstlog=archivehstlog.txt  #Historical archive History Log
+brmlog=brmlog.txt                #Display BRM LOG results
+brmlogtmp=brmlog.tmp             #Temp file
+archivebrmlog=archivebrmlog.txt  #Historical archive BRMS Log
+msgidtmp=msgidtmp.tmp
+msgidlist=msgidlist.txt
+emaillist="('ryan.cooper@siriuscom.com')"
+
+# Special Vars 
+numdays='1'                                                            #Number of days to search BRMS history for last save job
+tellme="CPFA09E BRM14A1 BRM10A1"                                       #Tell me how many times a MSGID appers in the Job Log
+#omitjoblog="'CPF0000'"                                                 #MSGIDs to ignore from the Job Log
+omitjoblog="BRM10A1|BRM14A1|BRM15A7|CPC2402|CPFA09E|CPD37C3|CPD384E"  #Example MSGIDs to ignore from the Job Log
+joblogsev='20'                                                         #Severity filter for the Job Log
+#omithstlog="'CPF0000'"                                                 #MSGIDs to ignore from the History Log
+omithstlog="'BRM14A1','BRM10A1'"                                      #Example MSGIDs to ignore from the History Log
+hstlogsev='20'                                                         #Severity filter for the History Log
+#omitbrmlog="'CPF0000'"                                                 #MSGIDs to ignore from the BRMS Log
+omitbrmlog="'BRM14A1','BRM10A1'"                                      #Example MSGIDs to ignore from the BRMS Log
+brmlogsev='20'                                                         #Severity filter for the BRMS Log
+
+# Cleanup of previous runs
+rm $ifsdir/$log
+rm $ifsdir/$out
+rm $ifsdir/$file1
+rm $ifsdir/$file2
+rm $ifsdir/$hstlog
+rm $ifsdir/$brmlog
+rm $ifsdir/$joblog
+rm $ifsdir/$hstlogtmp
+rm $ifsdir/$brmlogtmp
+rm $ifsdir/$joblogtmp
+rm $ifsdir/$msgidlist
+rm $ifsdir/$msgidtmp
+
+# Creating new files
+touch $ifsdir/$log
+touch $ifsdir/$out
+touch $ifsdir/$file1
+touch $ifsdir/$file2
+touch $ifsdir/$hstlog
+touch $ifsdir/$brmlog
+touch $ifsdir/$joblog
+touch $ifsdir/$archivejoblog
+touch $ifsdir/$archivehstlog
+touch $ifsdir/$archivebrmlog
+touch $ifsdir/$hstlogtmp
+touch $ifsdir/$brmlogtmp
+touch $ifsdir/$joblogtmp
+touch $ifsdir/$msgidlist
+touch $ifsdir/$msgidtmp
+
+# Setting prefereed CCSIDs
+setccsid 1208 $ifsdir/$log
+setccsid 1208 $ifsdir/$out
+setccsid 1208 $ifsdir/$file1
+setccsid 1208 $ifsdir/$file2
+setccsid 1208 $ifsdir/$hstlog
+setccsid 1208 $ifsdir/$brmlog
+setccsid 1208 $ifsdir/$joblog
+setccsid 1208 $ifsdir/$archivejoblog
+setccsid 1208 $ifsdir/$archivehstlog
+setccsid 1208 $ifsdir/$archivebrmlog
+setccsid 1208 $ifsdir/$hstlogtmp
+setccsid 1208 $ifsdir/$brmlogtmp
+setccsid 1208 $ifsdir/$joblogtmp
+setccsid 1208 $ifsdir/$msgidlist
+setccsid 1208 $ifsdir/$msgidtmp
+
+date >$log
+
+# Check for BRMS SQL Services enabled.
+system "DSPOBJD OBJ(QUSRBRM/LOG_INFO) OBJTYPE(*FILE)"
+if test $? == 1
+ then system "INZBRM OPTION(*SQLSRVINZ)"
+  if test $? == 1
+   then echo "Not able to INZ BRMS for SQL Services, Check PTF prereqs again"
+   exit
+   else echo "INZ of BRMS for SQL Services completed"
+  fi
+ else echo "BRMS SQL Services already enabled"
+fi
+
+echo "Checking job information override" >>$log
+if [[ "$1" == 'LAST' ]]
+ then
+ jobuc=`db2 "SELECT DISTINCT QUALIFIED_JOB_NAME, MESSAGE_TIMESTAMP FROM QUSRBRM.BRMS_LOG_INFO WHERE AREA ='BACKUP' AND MESSAGE_TIMESTAMP >= current timestamp - $numdays day ORDER BY MESSAGE_TIMESTAMP DESC LIMIT 1" | awk {'print $1'} | sed '4!d'`
+ else
+ if [[ -z "$1" ]]; then
+  echo "Input is Empty"; else
+  echo "Input is $1"; jobuc=$1;
+ fi
+fi
+
+if [[ -z "$jobuc" ]]
+ then
+  echo "No job information provided Running save" >>$log
+  system "STRBKUBRM CTLGRP($ctlg) SBMJOB(*yes)" >$out 2>&1
+  sleep 10
+  jobuc=`cat $out |grep CPC1221 | awk {'print $3'}`
+ else
+  echo "Job information provided skipping Save"
+fi
+
+echo "Setting Lower case job information" >>$log 
+joblc=`echo "$jobuc" | tr 'A-Z' 'a-z'`
+
+echo "Checking for active job $joblc" >>$log
+ps -e | grep $joblc >>$log
+
+while ps -e | grep $joblc
+  do
+  date  >>$log
+  echo "Job $joblc is still running, Checking again later" >>$log
+  sleep 10
+done
+
+echo "Job $joblc has completed" >>$log
+ps -e | grep $joblc >>$log
+
+echo "Display of history log for Job $jobuc sev X or greater" >>$log
+# Optionally add MESSAGE_SECOND_LEVEL_TEXT to below SQL statement
+db2 "SELECT MESSAGE_ID, SEVERITY, MESSAGE_TIMESTAMP, FROM_JOB, MESSAGE_TEXT FROM table(qsys2.history_log_info()) WHERE FROM_JOB ='$jobuc' AND MESSAGE_ID NOT IN("$omithstlog") AND SEVERITY >=$hstlogsev" >>$hstlog
+if [[ "$?" == '1' ]]
+ then hstlogresult='1'
+ else sed '/-----/d' $hstlog > $hstlogtmp && mv $hstlogtmp $hstlog
+fi
+
+echo "Display of BRMS LOG for job $jobuc Sev X or greater" >>$log
+# Optionally add MESSAGE_SECOND_LEVEL_TEXT to below SQL statement
+db2 "SELECT MESSAGE_ID, MESSAGE_SEVERITY, QUALIFIED_JOB_NAME, CONTROL_GROUP, MESSAGE_TEXT FROM QUSRBRM.BRMS_LOG_INFO WHERE QUALIFIED_JOB_NAME='$jobuc' AND MESSAGE_ID NOT IN("$omitbrmlog") AND SEVERITY >=$brmlogsev" >>$brmlog
+if [[ "$?" == '1' ]]
+ then brmlogresult='1'
+ else sed '/-----/d' $brmlog > $brmlogtmp && mv $brmlogtmp $brmlog
+fi
+
+echo "Display of Job log for job $jobuc" >>$log
+catsplf -j $jobuc QPJOBLOG 1 | sed '/5770SS1/d; /Job name/d; /Job description/d; /MSGID/d; /To module/d; /To procedure/d; /Statement ./d; /From module/d; /From procedure/d' >$file1
+cat $file1 | sed '
+/^[ ][A-Z]/{
+x
+/./ {
+x
+s/^/\
+/
+x
+}
+x
+}
+h' >$file2
+
+# Returns paragraph if SEV is greater then X and Omits MSGIDs defined above
+pgraph() {
+sed -e '/./{H;$!d;}' -e "x;/$1/!d" $file2 >>$joblog
+}
+
+cat $file2 | egrep "^[ ][A-Z]" | egrep -v $omitjoblog | awk -vm=${joblogsev} '$3 >= m' | awk '{ print $1 }' | sort | uniq | while read a
+do
+pgraph $a
+done
+
+
+# Check for empty logs to exclude from email
+fileatt=''
+
+echo $fileatt
+if [[ "$hstlogresult" == '1' ]]
+ then echo "No Histroy log information found matching critera"
+ else fileatt="$fileatt ($hstlog *OCTET *TXT)"
+fi
+if [[ "$brmlogresult" == '1' ]]
+ then echo "No BRMS Log information found matching critera"
+ else fileatt="$fileatt ($brmlog *OCT *TXT)"
+fi
+if [[ -s "$joblog" ]]
+ then fileatt="$fileatt ($joblog *OCT *TXT)"
+ else echo "No Job log information found matching critera"
+fi
+if [[ -s "$file1" ]]
+ then fileatt="$fileatt ($file1 *OCT *TXT)"
+ else echo "No Job log information found"
+fi
+if [[ -z "$fileatt" ]]
+ then echo "No informaton found. Assuming job completed without errors and is no longer on the system"
+ exit
+ else echo "Information found sending email"
+fi
+
+
+for fn in $tellme; do echo Number of times $fn occures " " |tr -d '\n'; grep $fn dsjoblog.txt | wc -l; done
+
+# List number of times each MSGID appers in Job Log
+cat $file2 | cut -c'2-8','37-38'| sort | uniq | egrep -v NONE |sed 's/./& /7'| awk '{print $1" "$2}'| sed '/^[ ]/d'| while read a b
+do
+echo Number of times $a with Severity $b occures " " |tr -d '\n' >>$msgidtmp; grep $a $file2 | wc -l  >>$msgidtmp
+done
+cat $msgidtmp | sort -k7 -r >$msgidlist
+
+# Scrub of files before emailing
+
+
+system "SNDSMTPEMM RCP($emaillist) SUBJECT('BRMS logs') NOTE('BRMS logs') ATTACH($fileatt)"
+
+# Copy current informtion into archive logs
+cat $joblog >> $archivejoblog
+cat $hstlog >> $archivehstlog
+cat $brmlog >> $archivebrmlog
+
+# Change log YYYY-MM-DD
+# 2021-12-20 Start of Change log and Version 2021.12.20
+# 2021-12-21 Added checks for empty files and no result sets
+#            If found exclued them from the Email.
+# 2021-12-22 Added several tmp files. Removed 2nd level text from SQL queries for BRM and HST log.
+#            Created archive files for future global logging and reporting
+# 2021-12-27 Added MSGIDLIST count process
